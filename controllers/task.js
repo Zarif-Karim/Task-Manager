15633ff7cfa1815b0c5c41f8dad63bfbeed02976
Zarif Karim <zarifkarim09@gmail.com>
2022-02-06 01:38:15 +1100

    Set task collection schema and implemented all the controller functions

--- controllers/task.js
+++ controllers/task.js
@@ -1,14 +1,58 @@
 const taskModel = require('../models/task');
 
-const get_all_tasks = (req,res) => { res.json(req.body); }
+const get_all_tasks = async (req,res) => { 
+    try{
+        const tasks = await taskModel.find({});
+        res.status(201).json({tasks}); 
+    } catch(error){
+        res.status(500).json({msg: error});
+    }
+}
 
 const add_task = async (req,res) => { 
-    const task = await taskModel.create(req.body);
-    res.status(201).json({task}); 
+    try{
+        const task = await taskModel.create(req.body);
+        res.status(201).json({task}); 
+    } catch(error){
+        res.status(500).json({msg: error});
+    }
 }
-const get_single_task = (req,res) => { res.send('getting single task'); }
-const update_single_task = (req,res) => { res.send('updating single task'); }
-const delete_single_task = (req,res) => { res.send('delete single task'); }
+
+const get_single_task = async (req,res) => { 
+    try{
+        const task = await taskModel.findById(req.params.id).exec();
+        if(!task){
+            return res.status(404).json({ msg: `No task found with id: ${req.params.id}` });
+        }
+        res.status(201).json({task}); 
+    } catch(error){
+        res.status(500).json({msg: error});
+    }
+}
+
+const update_single_task = async (req,res) => { 
+    try{
+        const task = await taskModel.findByIdAndUpdate(req.params.id,req.body,{runValidators:true});
+        if(!task){
+            return res.status(404).json({msg: `No task found with _id: ${req.params.id}`});
+        }
+        res.status(201).json({msg: 'Following Task updated', task}); 
+    } catch(error){
+        res.status(500).json({msg: error});
+    }
+}
+
+const delete_single_task = async (req,res) => { 
+    try{
+        const task = await taskModel.findByIdAndDelete(req.params.id).exec();
+        if(!task){
+            return res.status(404).json({msg: `No task found with _id: ${req.params.id}`});
+        }
+        res.status(201).json({msg: 'Following Task deleted', task}); 
+    } catch(error){
+        res.status(500).json({msg: error});
+    }
+}
 
 
 module.exports =  {
