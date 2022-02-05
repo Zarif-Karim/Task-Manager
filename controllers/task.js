6f2881e9debdff1c5118bdb8543a4eb0a5118c21
Zarif Karim <zarifkarim09@gmail.com>
2022-02-05 23:35:01 +1100

    Created Example tast Schema, then exported model for the schema. Refactored the add_task method in the controller(task.js) to add task in Atlas using the model

--- controllers/task.js
+++ controllers/task.js
@@ -1,6 +1,11 @@
+const taskModel = require('../models/task');
 
 const get_all_tasks = (req,res) => { res.json(req.body); }
-const add_task = (req,res) => { res.send('adding a task'); }
+
+const add_task = async (req,res) => { 
+    const task = await taskModel.create(req.body);
+    res.status(201).json({task}); 
+}
 const get_single_task = (req,res) => { res.send('getting single task'); }
 const update_single_task = (req,res) => { res.send('updating single task'); }
 const delete_single_task = (req,res) => { res.send('delete single task'); }
